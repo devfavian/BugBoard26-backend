a7d40cea29ad8b333782680ea01960e071949710
devfavian <devfavian@gmail.com>
2025-12-10 17:31:41 +0100

    Initial commit

---
+++ src/main/java/it/unina/bugboard/BugBoard26BackendApplication.java
@@ -0,0 +1,13 @@
+package it.unina.bugboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugBoard26BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BugBoard26BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/unina/bugboard/BugBoard26BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unina.bugboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugBoard26BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
